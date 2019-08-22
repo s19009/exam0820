8f6e1aa27183be5057fead2da2a7b27679874e49
s19009 <s19009@std.it-college.ac.jp>
2019-08-22 14:25:42 +0900

    GUItest上げ直し

--- GUI.py
+++ GUI.py
@@ -1,24 +1,33 @@
 from tkinter import Tk, Text, Button, Entry
 from tkinter import messagebox as mb
+import datetime
 
 
 root = Tk()
 root.title('あなたの名前は？')
+root.geometry("500x300")
 
-text = Entry(root)
-text.pack()
+text = Entry(width=30)
+text.place(x=125, y=100)
+
+time = datetime.datetime.now()
 
 
 def say_hello():
     a = text.get()
-    mb.showinfo('挨拶', a + 'さんこんにちは')
+    if time.hour <= 10:
+        mb.showinfo('挨拶', a + 'さんおはようございます')
+    elif time.hour >= 18:
+        mb.showinfo('挨拶', a + 'さんこんばんは')
+    else:
+        mb.showinfo('挨拶', a + 'さんこんにちは')
 
 
 button = Button(
     text="教える",
-    width=10, height=3,
+    width=10, height=2,
     command=say_hello
 )
-button.pack()
+button.place(x=200, y=200)
 
 root.mainloop()
